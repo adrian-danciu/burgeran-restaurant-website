c6b5c4e28aa69017c644e13eaea6256e533cbab4
Adrian Danciu <100218355+Crannkky@users.noreply.github.com>
2023-10-28 23:47:44 +0300

    fixed about us

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -8,27 +8,22 @@ const About = () => {
       <h1 className="font-subtitle text-primary text-5xl">
         Why ours are the best?
       </h1>
-      <div className="w-[90%] flex lg:flex-row flex-col lg:justify-around justify-center items-center bg-background p-10 rounded-xl">
+      <div className="w-[90%] max-w-[1200px] flex lg:flex-row flex-col lg:justify-around justify-center items-center bg-background p-10 rounded-xl lg:gap-0 gap-5 text-center">
         <p className="font-text w-80 text-xl leading-8 text-white">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet quam
-          voluptas nulla odit impedit, aperiam soluta sit tempora quasi quo
-          commodi molestias, doloremque officia. Optio non adipisci veritatis
-          excepturi, placeat repellendus ducimus fugit provident minima harum,
-          dolore, maxime iusto. Consectetur sit natus doloremque quod. Dolorem
-          et totam dolore esse perferendis.
+          Welcome to Burgeran – where the sizzle of gourmet burgers meets the
+          warmth of freshly toasted bread. Dive into a culinary journey that
+          celebrates the finest ingredients and handcrafted flavors, making
+          every bite an unforgettable experience.
         </p>
         <img className="rounded-xl" src={about2} alt="about2" />
       </div>
 
-      <div className="w-[90%] flex lg:flex-row flex-col lg:justify-around justify-center items-center bg-primary p-10 rounded-xl">
+      <div className="w-[90%] max-w-[1200px] flex lg:flex-row flex-col lg:justify-around justify-center items-center bg-primary p-10 rounded-xl lg:gap-0 gap-5 text-center">
         <img className="rounded-xl" src={about3} alt="about2" />
         <p className="font-text w-80 text-xl leading-8 text-white">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet quam
-          voluptas nulla odit impedit, aperiam soluta sit tempora quasi quo
-          commodi molestias, doloremque officia. Optio non adipisci veritatis
-          excepturi, placeat repellendus ducimus fugit provident minima harum,
-          dolore, maxime iusto. Consectetur sit natus doloremque quod. Dolorem
-          et totam dolore esse perferendis.
+          At Burgeran, we believe in the simple magic of a well-crafted burger
+          and the comforting touch of a golden toast. Join us in savoring
+          moments of delicious delight, one plate at a time.
         </p>
       </div>
     </section>
