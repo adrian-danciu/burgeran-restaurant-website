c1d29a860d19074ca79596e4cd7db7f197cf5f1e
Adrian Danciu <100218355+Crannkky@users.noreply.github.com>
2023-11-13 21:31:21 +0200

    adaugat buton pe homepage
    am adaugat un buton pe homepage comform ticket #12345

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,6 +14,7 @@ const Home = () => {
       <AboutBanner />
       <PromotionBanner />
       <ContactForm />
+      <button className="h-[50px] w-[200px] bg-black text-white">Exemplu buton</button>
     </div>
   );
 };
